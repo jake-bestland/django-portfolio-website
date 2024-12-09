a2e7f936f9da3b8db8d7846221916b2ae848c750
jake-bestland <beja0405@gmail.com>
2024-12-09 12:32:34 -0600

    Added some tests.

--- projects/tests.py
+++ projects/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Skill
+import unittest
+from django.core.mail import send_mail
+from django.core import mail
 
 # Create your tests here.
+# class ProjectModelTests(TestCase):
+#     def test_
+
+
+
+class PortfolioTests(TestCase):
+    def test_db_connection(self):
+        skills = Skill.objects.all()
+        self.assertIsNotNone(skills, msg="skills does not exist")
+
+    def test_send_email(self):
+        send_mail('Message from Portfolio contact page.',
+                  'This is the message',
+                  from_email='beja0405@gmail.com',
+                  recipient_list=['jake.bestland@gmail.com'],
+                  html_message = 'testing')
+        
+        self.assertEqual(len(mail.outbox),1,"No emails were sent.")
+
+if __name__ == "__main__":
+    unittest.main()
