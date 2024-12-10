83a1f21e6bdad0b46c0c42e4ac6e2b9cc4f17964
jake-bestland <beja0405@gmail.com>
2024-12-10 10:30:26 -0600

    Updated requirements file and set debug to False

--- jb_portfolio/settings.py
+++ jb_portfolio/settings.py
@@ -28,7 +28,7 @@ MEDIA_URL = '/media/'
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '35.153.170.78', 'localhost', 'jake-bestland.xyz']
 
